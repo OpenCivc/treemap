2ea757dca45219b10b06268e603989916b345054
Paul <paul@pogodan.com>
2016-02-15 19:48:16 -0500

    updates

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM ubuntu:12.04
-
-RUN apt-get install all_my_dependencies
-ADD my_app_files /my_app
-
-CMD ["/my_app/start.sh"]
--- tree-import.py
+++ tree-import.py
@@ -14,9 +14,9 @@ import treemap.species
 
 cursor = connection.cursor()
 
-Tree.objects.count()
-Plot.objects.count()
-# 106 = max built-in specie ID
+# Tree.objects.count()
+# Plot.objects.count()
+# # 106 = max built-in specie ID
 # Tree.objects.all().delete()
 # Plot.objects.all().delete()
 # cursor.execute("DELETE FROM treemap_species WHERE id > 106")
@@ -28,32 +28,31 @@ Plot.objects.count()
 
 # /usr/local/otm/app/opentreemap/ows.json
 # user = User.objects.all()[0]
-user = User.objects.get(username = 'demo')
+user = User.objects.get(username = 'pmeserve')
 city = Instance.objects.all()[0]
 f = open('ows.json', 'r')
 trees = json.load(f)
 
 # Species(instance=city).user_can_create(user)
 
-for s in treemap.species.SPECIES:
-  if Species.objects.filter(otm_code=s['otm_code']).exists():
-    print "Species %s already exists, skipping" % s['otm_code']
-    continue
-  print "creating species %s" % s['otm_code']
-  cursor.execute("INSERT INTO treemap_species (instance_id, max_diameter, max_height, udfs, genus, species, cultivar, common_name, otm_code) VALUES (1, 200, 800, '', %s, %s, %s, %s, %s)", [s['genus'], s['species'], s['cultivar'], s['common_name'], s['otm_code']])
+# for s in treemap.species.SPECIES:
+#   if Species.objects.filter(otm_code=s['otm_code']).exists():
+#     print "Species %s already exists, skipping" % s['otm_code']
+#     continue
+#   print "creating species %s" % s['otm_code']
+#   cursor.execute("INSERT INTO treemap_species (instance_id, max_diameter, max_height, udfs, flowering_period, fruit_or_nut_period, fact_sheet_url, plant_guide_url, genus, species, cultivar, common_name, otm_code, other_part_of_name) VALUES (%s, 200, 800, '', '', '', '', '', %s, %s, %s, %s, %s, %s)", [city.id, s['genus'], s['species'], s['cultivar'], s['common_name'], s['otm_code'], s['other_part_of_name']])
 
 #tree = trees['features'][0]
+# execfile('import.py')
+
 for tree in trees['features']:
   tp = tree['properties']
-  
   # broken
   if tp['id'] in [12265, 12266, 14157]: # "bad" trees w/ weird unicode/characters that won't import
     continue
-  
   if Tree.objects.filter(id=tp['id']).exists():
     print "Tree %s already exists, skipping" % tp['id']
     continue
-  
   print "creating tree: %s : %s" % (tp['id'], tree['geometry']['coordinates'])
   geom = Point(tree['geometry']['coordinates'], srid=4326)
   geom.transform(3857)
@@ -62,26 +61,30 @@ for tree in trees['features']:
   species = None
   date_planted = None
   date_removed = None
-  
+  canopy_height = 0.1
+  diameter = 0.1
   if tp['species_id'] is not None:
     print "species: %s, %s, %s" % (tp['scientific_name'], tp['common_name'], tp['species_id'])
-    
     try:
       species = Species.objects.get(common_name = tp['common_name'])
     except:
       print "couldn't find species"
       try:
-        cursor.execute("INSERT INTO treemap_species (instance_id, max_diameter, max_height, udfs, genus, common_name, otm_code) VALUES (1, 200, 800, '', %s, %s, %s)", [tp['scientific_name'], tp['common_name'], tp['species_id']])
+        cursor.execute("INSERT INTO treemap_species (instance_id, max_diameter, max_height, udfs, flowering_period, fruit_or_nut_period, fact_sheet_url, plant_guide_url, genus, species, cultivar, common_name, otm_code, other_part_of_name) VALUES (%s, 200, 800, '', '', '', '', '', %s, %s, %s, %s, %s, %s)", [city.id, tp['scientific_name'], '', '', tp['common_name'], tp['species_id'], ''])
         species = Species.objects.get(otm_code = tp['species_id'])
       except:
         print "exception"
-  
   if tp['date_planted'] is not None:
     date_planted = tp['date_planted'].split()[0]
   if tp['date_removed'] is not None:
     date_removed = tp['date_removed'].split()[0]
-    
-  t = Tree(id=tp['id'], diameter=tp['dbh'], height=tp['height'], canopy_height=tp['canopy_height'], date_planted=date_planted, date_removed=date_removed, plot=plot, instance=city, species=species)
+  if tp['canopy_height'] > 0:
+    canopy_height = tp['canopy_height']
+  if tp['dbh'] > 0:
+    diameter = tp['dbh']
+  if tp['height'] > 0:
+    height = tp['height']
+  t = Tree(id=tp['id'], diameter=diameter, height=height, canopy_height=canopy_height, date_planted=date_planted, date_removed=date_removed, plot=plot, instance=city, species=species)
   t.save_with_user(user)
 
 max_plot_id = Plot.objects.all().aggregate(Max('id'))['id__max']
